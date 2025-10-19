fc9e026a74e05e60517fa94f0f31e3ea3f11e835
LQCpaka <boyrovip1539@gmail.com>
2025-10-20 00:54:33 +0700

    handlers

--- backend/handlers/fileDialog_handler.go
+++ backend/handlers/fileDialog_handler.go
@@ -2,9 +2,32 @@ package handlers
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+type FileHandler struct{}
+
+func NewFileHandler() *FileHandler {
+	return &FileHandler{}
+}
+
+func (f *FileHandler) GetFirstFile(ctx context.Context, folderPath string) (string, error) {
+	entries, err := os.ReadDir(folderPath)
+	if err != nil {
+		return "", err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			return filepath.Join(folderPath, entry.Name()), nil
+		}
+	}
+
+	return "", nil
+}
 
 func OpenFileDialog(ctx context.Context) (string, error) {
 	file, err := runtime.OpenFileDialog(ctx, runtime.OpenDialogOptions{
